e5757d5bab0860d531168047b59ab0dc5c9bad89
ADMTIC30 <ADMTIC30@srs.local>
2020-08-20 23:51:00 -0500

    Pipe para variables globales

---
+++ src/app/pipes/global.pipe.ts
@@ -0,0 +1,22 @@
+import { Pipe, PipeTransform } from '@angular/core';
+import { global } from '../services/services.index';
+
+@Pipe({
+	name: 'global'
+})
+export class GlobalPipe implements PipeTransform {
+	transform(id: number, param: any): any {
+		let result = '-';
+		global[param].forEach(element => {
+			if ( element.id === id && (element.name || element.value) ) {
+				if (element.name) {
+					result = element.name;
+				}
+				if (element.value) {
+					result = element.value;
+				}
+			}
+		});
+		return result;
+	}
+}
--- src/app/pipes/pipes.module.ts
+++ src/app/pipes/pipes.module.ts
@@ -1,15 +1,18 @@
 import { NgModule } from '@angular/core';
 
 // Pipes
+import { GlobalPipe } from './global.pipe';
 import { IsNullFillPipe } from './is-null-fill.pipe';
 
 @NgModule({
 	declarations: [
+		GlobalPipe,
 		IsNullFillPipe,
 	],
 	imports: [
 	],
 	exports: [
+		GlobalPipe,
 		IsNullFillPipe,
 	],
 })
