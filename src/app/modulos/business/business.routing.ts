afa3e2a2fcd132a7fc3528e2e2a3fb7bec707ffb
ADMTIC30 <ADMTIC30@srs.local>
2020-09-03 10:03:08 -0500

    Business Inteligent Guards

--- src/app/modulos/business/business.routing.ts
+++ src/app/modulos/business/business.routing.ts
@@ -2,9 +2,17 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { BusinessDashboardComponent } from './dashboard/business-dashboard.component';
 
+// Guards
+import { BusinessDashboardGuard } from './guards/business-dashboard.guard';
+
 const businessRoutes: Routes = [
 	{ path: '', redirectTo: 'dashboard', pathMatch: 'full' },
-	{ path: 'dashboard', component: BusinessDashboardComponent, data: { titulo: 'Business Dashboard' } },
+	{
+		path: 'dashboard',
+		component: BusinessDashboardComponent,
+		data: { titulo: 'Business Dashboard' },
+		canActivate: [BusinessDashboardGuard]
+	},
 ];
 
 @NgModule({
---
+++ src/app/modulos/business/guards/business-dashboard.guard.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+import { Router, CanActivate } from '@angular/router';
+
+// Services
+import { UserService } from '../../../services/user.service';
+
+@Injectable({
+	providedIn: 'root'
+})
+export class BusinessDashboardGuard implements CanActivate {
+
+	constructor(
+		private userService: UserService,
+		private router: Router
+	) {}
+
+	canActivate() {
+		const identity = this.userService.getIdentity();
+
+		if (identity) {
+			const permissions = JSON.parse(localStorage.getItem('userOperations'));
+			for ( const permission of permissions ) {
+				if ( permission.id_operations === 65 ) { // Operación que permite entrar al dashboard de Business Inteligent
+					return true;
+				}
+			}
+			this.router.navigate(['/inicio']);
+			return false;
+		} else {
+			this.router.navigate(['/inicio']);
+			return false;
+		}
+	}
+}
