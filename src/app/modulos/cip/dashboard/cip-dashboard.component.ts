4151bb5072fc329269d5f690c4e3dbec36529213
ADMTIC30 <ADMTIC30@srs.local>
2020-09-28 13:11:46 -0500

    Tablero CIP - Pantalla General

---
+++ src/app/modulos/cip/dashboard/cip-dashboard.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cip-dashboard',
+  templateUrl: './cip-dashboard.component.html',
+  styles: []
+})
+export class CipDashboardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
