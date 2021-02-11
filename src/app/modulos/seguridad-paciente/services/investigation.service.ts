754765e6b1a33b5efdc891191d7af97505edb6d2
Santiago Ramirez Gaitan <santiagooo42@gmail.com>
2021-02-11 10:25:20 -0500

    Siasur-Seguridad del Paciente-Investigación del suceso de seguridad

---
+++ src/app/modulos/seguridad-paciente/services/investigation.service.ts
@@ -0,0 +1,49 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { global, UserService } from '../../../services/services.index';
+
+@Injectable({
+	providedIn: 'root'
+})
+export class InvestigationService {
+	url: string;
+	token: string;
+
+	constructor(
+		private http: HttpClient,
+		private userService: UserService,
+	) {
+		this.url = global.url;
+		this.token = this.userService.getToken();
+	}
+	
+	getInvestigationByEventId( eventId ): Observable<any> {
+		let headers = new HttpHeaders().set( 'Authorization', this.token );
+
+		return this.http.get( this.url + 'investigation/get-event/' + eventId, { headers } );
+	}
+
+	newInvestigation( investigation ): Observable<any> {
+		let json = JSON.stringify( investigation );
+		let params = 'json=' + json;
+		let headers = new HttpHeaders().set( 'Authorization', this.token )
+									   .set( 'Content-Type', 'application/x-www-form-urlencoded' );
+
+		return this.http.post( this.url + 'investigation', params, { headers } );
+	}
+
+	//==========================================================================
+	//===================Pre Clasification Events Documents=====================
+	//==========================================================================
+	deleteFile( filename ): Observable<any> {
+		let headers = new HttpHeaders().set( 'Authorization', this.token );
+
+		return this.http.delete(this.url + 'investigation/delete-file/' + filename, { headers } );
+	}
+	downloadEventDocument( filename ): Observable<any> {
+		let headers = new HttpHeaders().set( 'Authorization', this.token );
+
+		return this.http.get(this.url + 'investigation/get-file/' + `${filename}`, { responseType: 'blob', headers });
+	}
+}
