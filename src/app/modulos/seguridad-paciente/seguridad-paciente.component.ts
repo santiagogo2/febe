bbfa9e67f8d118030f7f14695290a8e05e927a2e
Santiago Ramirez Gaitan <santiagooo42@gmail.com>
2021-02-11 10:25:53 -0500

    Siasur-Seguridad del Paciente-Página inicial

---
+++ src/app/modulos/seguridad-paciente/seguridad-paciente.component.ts
@@ -0,0 +1,44 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-seguridad-paciente',
+  templateUrl: './seguridad-paciente.component.html',
+  styles: []
+})
+export class SeguridadPacienteComponent implements OnInit {
+	public identity: any;
+
+	public securityEvent: any[] = [
+		{ titulo: 'Preclasificacion', url: './preclasificacion', class: 'card-4FB96F', description: 'Preclasificación de los Reportes de Seguridad del Paciente' },
+		{ titulo: 'Investigación', url: './investigacion', class: 'card-EC8993', description: 'Investigación de los Reportes de Seguridad del Paciente' },
+		{ titulo: 'Nueva Encuesta', url: './encuesta', class: 'card-924E4E', description: 'Nueva Encuesta relacionada a un Reporte de Seguridad del Paciente' },
+		{ titulo: 'Generar Reporte', url: './reporte', class: 'card-ECC350', description: 'Reporte provisional de Seguridad del Paciente' },
+	];
+
+	constructor() {
+		this.validatePermissions();
+	}
+
+	ngOnInit(): void {
+	}
+
+	validatePermissions() {
+		const permissions = JSON.parse(localStorage.getItem('userOperations'));
+		const array = [];
+		permissions.forEach( element => {
+			if ( element.id_operations === 82 ) {
+				array.push(this.securityEvent[0]);
+			}
+			if ( element.id_operations === 83 ) {
+				array.push(this.securityEvent[1]);
+			}
+			if ( element.id_operations === 84 ) {
+				array.push(this.securityEvent[2]);
+			}
+			if ( element.id_operations === 72 ) {
+				array.push(this.securityEvent[3]);
+			}
+		});
+		this.securityEvent = array;
+	}
+}
