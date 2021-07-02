e7ef5bad8ef71e3f4eb598b7d56503ffcaa7b786
Santiago Ramirez Gaitan <santiagooo42@gmail.com>
2021-07-02 17:43:00 -0500

    Modelos del módulo de vacunación

---
+++ src/app/modulos/vacunacion/models/calls.ts
@@ -0,0 +1,15 @@
+export class Calls {
+	constructor(
+		public id: number,
+		public numeroDocumento: string,
+		public tipoDocumento: string,
+		public primerNombre: string,
+		public segundoNombre: string,
+		public primerApellido: string,
+		public segundoApellido: string,
+		public edad: number,
+		public aseguradora: number,
+		public celular: number,
+		public novedad: number,
+	){}
+}
